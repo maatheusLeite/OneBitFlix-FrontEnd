5bff348af6c7c3b6b23873d6848619f920fc6e58
Matheus Leite <maatheus2710@gmail.com>
2024-01-03 22:36:35 -0300

    Project created

---
+++ src/pages/index.tsx
@@ -0,0 +1,13 @@
+import Head from "next/head"
+import styles from "../styles/HomeNoAuth.module.scss"
+
+export default function homeNoAuth() {
+    return(
+        <>
+        <Head>
+            <title> OneBitFlix - Home </title>
+        </Head>
+        <main></main>
+        </>
+    )
+}
