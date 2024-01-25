d8334dc6bb2f6e2e16ae5a3322c1d6d8aa994535
Matheus Leite <maatheus2710@gmail.com>
2024-01-24 21:55:37 -0300

    Adding CardsSection

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 import HeaderNoAuth from "@/src/components/HomeNoAuth/HeaderNoAuth/HeaderNoAuth";
 import PresentationSection from "@/src/components/PresentationSection/PresentationSection";
 import styles from "../styles/HomeNoAuth.module.scss"
+import CardsSection from "@/src/components/HomeNoAuth/CardsSection/CardsSection";
 
 export default function HomeNotAuth() {
     return (
@@ -10,6 +11,7 @@ export default function HomeNotAuth() {
                     <HeaderNoAuth />
                     <PresentationSection />
                 </div>
+                <CardsSection />
             </main>
         </>
     )
